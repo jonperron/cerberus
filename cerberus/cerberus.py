1f03dbe97d2350c195401c02acdb3833d73aa4c6
Jonathan Perron <contact@jonathanperron.fr>
2018-05-07 13:50:09 +0000

    Updated third head

--- cerberus/cerberus.py
+++ cerberus/cerberus.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 from multiprocessing import Queue, Pool
 from cerberus.firsthead import FirstHead
 from cerberus.secondhead import SecondHead
+from cerberus.thirdhead import ThirdHead
 
 class Cerberus:
     """
@@ -15,6 +16,7 @@ class Cerberus:
     def __init__(self, *args, **kwargs):
         self.firsthead = FirstHead()
         self.secondhead = SecondHead()
+		self.thirdhead = ThirdHead()
 		
     def worker(self, input_q):
         # Load a (frozen) Tensorflow model into memory.
@@ -32,16 +34,18 @@ class Cerberus:
             frame = input_q.get()
             frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             objects = self.secondhead.detect_objects(frame_rgb, sess, detection_graph)
-            print(objects)
+            if 'person' in objects.keys():
+				self.thirdhead.new_detection()
 
         sess.close()
 
     def guard(self, *args, **kwargs):
+        self.thirdhead.update_status('Starting')
         input_q = Queue(maxsize=1)
         pool = Pool(2, self.worker, (input_q,))
 
         video_capture = self.firsthead.start()
-        
+        self.thirdhead.update_status('Guarding')
         while True:
             ret, frame = video_capture.read()
             input_q.put(frame)
--- cerberus/thirdhead.py
+++ cerberus/thirdhead.py
@@ -1,10 +1,68 @@
 #! /usr/bin/python3
 
+import json
 import requests
+import smtplib
+
+from email.message import EmailMessage
+from gunicorn.app.wsgiapp import WSGIApplication
 
 class ThirdHead:
-	"""
-	Third Head : Alerting.
+    """
+    Third Head : Barking.
+    
+    Alert using external server.
+    """
+    def __init__(
+        self,
+        app,
+        email_host='localhost',
+        email_port=None,
+        email_login=None,
+        email_password=None,
+        email_recipients=None,
+        *args,
+        **kwargs
+    ):
+        self.app = self.start_webserver()
+        self.base_url = 'http://localhost:8000/'
+        self.email_host = email_host        
+        self.email_login = email_login
+        self.email_password = email_password
+        self.email_recipients = email_recipents
+        self.request = self.get_requests_session()
 	
-	Alert using external server.
-	"""
+    def start_webserver(self):
+        app = WSGIApplication()
+        app.app_uri = 'cerberus.webserver:app'
+        return app.run()
+
+    def get_requests_session():
+        # Not really useful for the moment, but could be in the future with Auth system
+        return requests.Session()
+
+    def update_status(self, new_status):
+        self.request.post(self.base_url + 'cerberus_status', json.dumps({'status': new_status}))
+		
+    def new_detection(self):
+        self.request.post(self.base_url + 'new_detection')
+        self.request.post(self.base_url + 'detection_amount')
+        self.bark()
+        self.update_status('Intruder has been spotted !')
+        
+    def bark(self):
+        # Create message
+        msg = EmailMessage()
+        msg['From'] = 'cerberus@localhost'
+        msg['To'] = self.recipients
+        msg['Subject'] = 'New alert'
+        msg.set_content('An intruder has been spotted !')
+
+        # Barking !
+        with smtplib.SMTP(self.email_host, self.email_port) as smtp_server:
+            if self.email_login and self.email_password:
+                smtp_server.ehlo()
+                smtp_server.starttls()
+                smtp_server.login(self.email_login, self.email_password)
+            finally:
+                smtp_server.send_message(msg)
